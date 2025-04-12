08aad3a35043a8436bf32dd2cd04d3f605c6083c
Lalith-47 <Lalith_kumar_47@outlook.com>
2025-04-12 21:38:45 +0530

    Debugging the program

--- Patterns.py
+++ Patterns.py
@@ -1,4 +1,5 @@
 def pattern(a):
-  for i range(a):
-      print(i*'*')
-a=int(input("Enter number of lines: '))
+  for i in range(a+1):
+      print(i*str(i))
+a=int(input('Enter number of lines: '))
+pattern(a)
