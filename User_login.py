f26f25342786f501f9ed2898e1ee99bc27c1a559
Lalith-47 <Lalith_kumar_47@outlook.com>
2025-04-19 18:05:01 +0530

    Refactor login and signup functions for improved user input handling

--- User_login.py
+++ User_login.py
@@ -1,29 +1,63 @@
 database={'Alice':'abc123','Bankai':'Zenpacto'}
 import random
 import sys
-decision=int(input('Enter 1 for existing account\nEnter 2 to create account\n'))
-if decision==1:
-    username=input('Enter your username: ')
-    if username not in database:
-        print('Incorrect Username')
-        sys.exit(1)
-    password=input('Enter Password: ')
-    if password!=database[username]:
-        print('Invalid Password')
-        sys.exit(0)
-    OTP=random.randint(111111,999999)
-    print(OTP)
-    otp=int(input('Enter OTP: '))
-    if otp!=OTP:
-        print('Incorrect OTP')
-    else:
-        print('Access Approved')
-
-elif decision==2:
-    new_username=input('Enter new username: ')
+def login():
+    i=0
+    while i<=3:
+        if i==3:
+            print('Max limit reached')
+            sys.exit(0)
+        username=input('Enter your username: ')
+        if username not in database:
+            print('Incorrect Username')
+        else:
+            break
+        i+=1
+    i=0
+    while i<=3:
+        if i==3:
+            print('Max limit reached')
+            sys.exit(0)
+        password=input('Enter Password: ')
+        if password!=database[username]:
+            print('Invalid Password')
+        else:
+            break
+        i+=1
+    i=0
+    while i<=3:
+        if i==3:
+            print('Max limit reached')
+            sys.exit(0)
+        OTP=random.randint(111111,999999)
+        print(OTP)
+        otp=int(input('Enter OTP: '))
+        if otp!=OTP:
+            print('Incorrect OTP')
+        else:
+            print('Access Approved')
+            break
+        i+=1
+def signup():
+    while True:
+        new_username=input('Enter new username: ')
+        if new_username in database:
+            print('Username already exists')
+        else:
+            break
     new_password=input('Enter New password: ')
     database[new_username]=new_password
     print('Username: ',new_username,'\nPassword: ',database.get(new_username))
-else:
-    print('Invalid input')
-    sys.exit(1)
+def invalid():
+    print('Invalid Input')
+def main():
+    while True:
+        decision=input('Do you want to login or signup? (login/signup): ').lower()
+        if decision=='login':
+            login()
+        if decision=='signup':
+            signup()  
+        else:
+            invalid()
+            continue
+main()  
