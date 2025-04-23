773d86aec649cee44aa3d733b0b22c013465df82
Lalith-47 <Lalith_kumar_47@outlook.com>
2025-04-23 06:27:27 +0530

    Remove: Delete power function implementation from Exponent.py

--- Exponent.py
+++
@@ -1,8 +0,0 @@
-def power():
-    base=int(input('Enter base: '))
-    power=int(input('Enter Power: '))
-    value=1
-    for i in range(power):
-        value*=base
-    print('Output:',value)
-power()
