bd242ef61314159aba4c1c821bc583c8e045bb0d
Lalith-47 <Lalith_kumar_47@outlook.com>
2025-05-22 04:27:56 +0530

    Add: Create pprtint.py to demonstrate pretty printing of a dictionary

---
+++ pprtint.py
@@ -0,0 +1,9 @@
+from pprint import pprint as pp
+d = {
+    'Lalith': 'student',
+    'marks': [85, 90, 78],
+    'details': ('male', 21),
+    'passed': True,
+    'address': {'city': 'Bangalore', 'zip': 560064}
+}
+pp(d)
