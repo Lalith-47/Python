016eede1ceb7ba4b06ee3e3d7473c503770e47f7
Lalith-47 <Lalith_kumar_47@outlook.com>
2025-06-22 21:12:53 +0530

    Updation to typos in programs

--- Timer.py
+++ Timer.py
@@ -1,8 +1,7 @@
 import time
-from time import strftime
 
 n=int(input('Enter timer to stop: '))
 for i in range(n,-1,-1):
     time.sleep(1)
-    print(f"{i} seconds lest",end='\r',flush=True)
-print(f"Timer Stopped at {strftime("%I:%M:%S %p",time.localtime())}")
+    print(f"{i} seconds left",end='\r',flush=True)
+print(f"Timer Stopped at {time.strftime("%I:%M:%S %p",time.localtime())}")
